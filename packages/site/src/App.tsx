acae935339dca4376474f18e38dbb31e13047346
Johanna Fransson <adrina0002@gmail.com>
2023-05-13 18:04:33 +0100

    remove footer

--- packages/site/src/App.tsx
+++ packages/site/src/App.tsx
@@ -26,7 +26,6 @@ export const App: FunctionComponent<AppProps> = ({ children }) => {
       <Wrapper>
         <Header handleToggleClick={toggleTheme} />
         {children}
-        <Footer />
       </Wrapper>
     </>
   );
